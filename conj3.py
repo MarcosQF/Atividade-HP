8e5e86270f43fe50b5c818ed3dabfe3feca1005c
Marcos Queiroz <marcosamfq123@gmail.com>
2024-11-10 20:43:19 -0300

    criação conj3

---
+++ conj3.py
@@ -0,0 +1,59 @@
+from heap import *
+
+#Conjunto 3
+
+valores = [50, 40, 30, 20, 10, 5, 3]
+
+bh1 = Heap()
+
+root = tk.Tk()
+root.title("Heap Tree")
+canvas = tk.Canvas(root, width=800, height=600)
+canvas.pack()
+
+#1. Construção do Heap
+
+for valor in valores:
+    bh1.insert(valor)
+    bh1.display_heap(canvas) 
+
+#2. Alteração de Prioridade
+
+root.after(700)  
+bh1.change_priority(2, 60) 
+bh1.display_heap(canvas)  
+
+root.after(700)  
+bh1.change_priority(5,1)
+bh1.display_heap(canvas) 
+
+#3. Remoções
+
+root.after(700) 
+bh1.remove()
+canvas.delete("all") 
+bh1.display_heap(canvas)
+root.update()
+
+bh1.remove()
+canvas.delete("all") 
+bh1.display_heap(canvas)
+root.update()
+
+bh1.remove()
+canvas.delete("all") 
+bh1.display_heap(canvas)
+root.update()
+
+#4. Heapsort
+
+root.after(700) 
+bh1.heap_sort(canvas,root)
+bh1.display_heap(canvas)
+root.update()
+
+#5. Selecionar Alta Prioridade
+print(bh1.get_high_priority())
+
+
+root.mainloop()
